1627b65e1731be40938b94054212b54471b5af48
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2026-04-19 17:10:18 -0600

    Adding some launch scripts

---
+++ launch_scripts/gpu-dist-high-memory.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#SBATCH --time=00-00:10:00
+
+#SBATCH --nodes=2
+#SBATCH --ntasks=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH -p kingspeak-gpu
+#SBATCH -A kingspeak-gpu
+
+#SBATCH -o slurmjob-%j@%N.out #stdout file in format slurmjob-SLURM_JOB_ID.out-NODEID
+
+cd /uufs/chpc.utah.edu/common/home/u6074057/classes/cs5030/Flow
+
+echo "---INIT ENVIRONMENT---"
+module load cuda/11.8.0 openmpi/4.1.6-gpu gdal && export OMPI_MCA_opal_cuda_support=1 && ulimit -l unlimited
+
+echo "---COMPILE---"
+nvcc -c --std=c++17 cuda-distributed.cu -o cuda-distributed.o && mpicxx cuda-distributed.o -o cuda-distributed -lcudart -lgdal -lstdc++fs
+
+echo "---cache_valley.tif 100 25 (2 GPUs)---"
+time mpiexec -n 2 cuda-distributed ../elevation-maps/cache_valley.tif 100 25
